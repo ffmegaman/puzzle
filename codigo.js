47e343cd6ff8e3a6c1f7bcea6d8435d5a7e6d3a4
DonQuality <santosemmanuel@outlook.com>
2014-08-22 12:18:47 -0700

    Front page design and Jquery updates-ES

---
+++ codigo.js
@@ -0,0 +1,31 @@
+$('#puzzle-start, #play_button').click(function(){
+  $('#main-puzzle-box').show(function(){
+    $(this).click().animate({
+      "width":"100%",
+      "display":"block"
+    },'fast');
+  });
+});
+
+$('#puzzle-start').hover(function(){
+  $('#main-puzzle-box').show(function(){
+    $(this).click().animate({
+      "width":"100%",
+      "display":"block"
+    },'fast');
+  });
+});
+
+$('#main-header').mouseenter(function(){
+  $('#main-header').animate({
+    "opacity":".8"
+  });
+});
+
+$('#main-puzzle-box').dblclick(function(){
+  $('#main-puzzle-box').hide(function(){
+    $(this).fadeOut()
+  });
+});
+
+$('img').draggable();
