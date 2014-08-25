a86ef42c920f884e032f7295c16e5ca5e7b4bec3
Kevin Trottier <ktrottier95@gmail.com>
2014-08-25 16:07:27 -0700

    Heckler alerts

--- timer.js
+++ timer.js
@@ -37,3 +37,12 @@ $(document).ready(function(){
     }
   }
 });
+function show(document.getElementById('popup').style.display='block';){
+  $('#popup').html("Hurry up, Noob!");
+}
+//Heckler messages, can be inserted where appropriate.
+setTimeout(show(), 60000);
+
+setTimeout(function() {alert("My elderly grandma finished this faster than you!")}, 120000);
+
+setTimeout(function() {alert("You should just stop, you have the puzzle-solving skills of a carrot.")}, 180000);
