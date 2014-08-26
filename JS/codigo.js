b851189234d0f5ebfb5e77c89c975c03a1275daf
Philip Nguyen <supertaru@gmail.com>
2014-08-26 15:44:18 -0700

    Update shuffle for on play click

--- JS/codigo.js
+++ JS/codigo.js
@@ -67,20 +67,23 @@ $(document).ready(function(){
   $('.puzzle-set-1').sortable();
   $('#puzzle-set-2').sortable();
 
-function shuffleArray(o){
-    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-}
+  // This hides the puzzle set
+  $('#puzzle-set-2').hide();
 
-function insertShuffled(puzzleSet) {
-  var pieces = $(puzzleSet + '> li');
-  $(puzzleSet).empty();
-  $.each(shuffleArray(pieces), function(index, piece){
-    $(puzzleSet).append(piece);
-  });
-}
+  function shuffleArray(o){
+      for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+  }
 
-insertShuffled('.puzzle-set-1');
+  function insertShuffled(puzzleSet) {
+    var pieces = $(puzzleSet + '> li');
+    $(puzzleSet).empty();
+    $.each(shuffleArray(pieces), function(index, piece){
+      $(puzzleSet).append(piece);
+    });
+  }
+
+  $('puzzle-set-1').on('click', insertShuffled('.puzzle-set-1'), false);
 
 
 });
