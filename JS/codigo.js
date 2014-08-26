c9192fc07d7e1f9b6e407b3bcf0208663d33ec08
DonQuality <santosemmanuel@outlook.com>
2014-08-26 13:05:02 -0700

    updates

--- JS/codigo.js
+++ JS/codigo.js
@@ -12,8 +12,7 @@ $(document).ready(function(){
     $('#main-about-box').show(function(){
       $(this).click().animate({
       "height":"31.9em",
-      "display":"block",
-      "z-index":"1000"
+      "display":"block"
       },'slow');
     });
   });
@@ -26,20 +25,47 @@ $(document).ready(function(){
     $('#main-puzzle-box').fadeOut('slow');
   });
 
-  $('#main-header').hover(function(){
+  $('#main-header').mouseenter(function(){
     $('#main-header').animate({
-      "opacity":".9"
+      "opacity":"1"
+      });
     });
-  });
 
-  $('#play_button, #puzzle-start').click(function(){
-    $('#main-header').animate({
+  $('#main-header').mouseleave(function(){
+    $(this).animate({
       "opacity":"1"
     });
   });
 
+  $('#play_button, #puzzle-start, #click-about').click(function(){
+    $('#main-header').animate({
+      "opacity":"1"
+    }, 'fast');
+  });
+
+  $('li').hover(function(){
+    $(this).animate({
+      "opacity":"1"
+    }, 'fast');
+  });
+
+  $('#play_button, #puzzle-start').click(function(){
+    $('#your-status').slideToggle("fast", function(){
+      $('#your-status').animate({
+        "width":"18%",
+        "opacity":"1",
+        "z-index":"998"
+      }, 'fast');
+    });
+  });
+
+  $('#main_logo').on('click', function(){
+    $('#your-status').fadeOut();
+  });
+
+
   $('.puzzle-set-1').sortable();
-  $('.puzzle-set-2').sortable();
+  $('#level2__puzz').sortable();
 
 
 });
